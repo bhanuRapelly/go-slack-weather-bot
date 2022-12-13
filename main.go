568acc0e60494d367dee800c58a1fc82f2bd91cf
bhanu <bhanutezryuga@gmail.com>
2022-12-13 20:22:54 +0530

    base weather bot complete

--- main.go
+++ main.go
@@ -2,20 +2,59 @@ package main
 
 import (
 	"context"
-	"log"
-	//"encoding/json"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
-	"github.com/shomali11/slacker"
 	"github.com/lpernett/godotenv"
-	//"github.com/tidwall/gjson"
+	"github.com/shomali11/slacker"
 )
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func weatherQuery(city string) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + os.Getenv("OPENWEATHERMAP_API_KEY") + "&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+	defer resp.Body.Close()
+
+	var d weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+	return d, nil
+}
+
+func convertKtoC(tempK float64) float64 {
+	tempC := tempK - 273.15
+	return tempC
+}
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+	}
+}
 
 func main() {
 	godotenv.Load(".env")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
 
 	bot.Command("query - <city>", &slacker.CommandDefinition{
 		Description: "enter city name",
@@ -23,8 +62,16 @@ func main() {
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			query := request.Param("city")
 
-			fmt.Println(query)
-			response.Reply(query)
+			data, err := weatherQuery(query)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(data.Main.Kelvin)
+			tempC := convertKtoC(data.Main.Kelvin)
+			fmt.Println(tempC)
+
+			tempstr := fmt.Sprintf("%.3f", tempC) + "C degrees"
+			response.Reply(tempstr)
 		},
 	})
 
